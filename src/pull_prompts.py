3effac2b6c3b810ec77bc5f4d39b88a32ffc62eb
Rogerio Martins <rogerio.martins@ima.sp.gov.br>
2026-03-15 10:14:05 -0300

    Pull do prompt -> OK

--- src/pull_prompts.py
+++ src/pull_prompts.py
@@ -15,17 +15,31 @@ from pathlib import Path
 from dotenv import load_dotenv
 from langchain import hub
 from utils import save_yaml, check_env_vars, print_section_header
+from langchain_core.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate
+import yaml
 
 load_dotenv()
 
 
-def pull_prompts_from_langsmith():
-    ...
+def pull_prompts_from_langsmith(output_path: str):
+    username = os.getenv("USERNAME_LANGSMITH_HUB")
+    prompt_name = os.getenv("PROMPT_NAME")
+    prompt_identifier = f"{username}/{prompt_name}"
 
+    print(f"Buscando prompt: {prompt_identifier}...")
+    prompt = hub.pull(prompt_identifier)
+    output_dir = os.path.dirname(output_path)
+    if output_dir:
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+    
+    save_yaml(prompt, output_path)
+    print(f"✅ Prompt salvo em YAML: {output_path}")
+   
 
 def main():
     """Função principal"""
-    ...
+    output_path = "prompts/raw_prompts.yml"
+    pull_prompts_from_langsmith(output_path)
 
 
 if __name__ == "__main__":
